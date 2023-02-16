f67eb4441c98d0ce003c148498bfd2fae0a6771a
HawiTad <adugtad@gmail.com>
2023-02-16 15:16:33 +0300

    pageHero fixes

--- pages/homepage.js
+++ pages/homepage.js
@@ -14,88 +14,43 @@ const Homepage = () => {
 
 
     const Slide1 = () => {
-        const swiper = useSwiper();
         return (
-            <div className='self-center md:self-start md:ml-[70px]' >
-                <div className='self-center text-center md:text-start md:self-start  md:pl-[28px] mt-3 md:mt-4 w-full'>
+            <div className='self-center lg:self-start' >
+                <div className='flex flex-col md:items-center lg:items-start self-center text-center lg:text-start lg:self-start mt-3 lg:mt-4 w-full'>
                     <span className='text-white text-[60px] tb:text-[75px] font-bold leading-none'>ALL YOU NEED <br />TO BE EPIC</span>
-                    <p className='md:max-w-[550px] text-[17px] md:text-[20px] text-white mt-10 px-4 md:px-[0px] md:mt-5'>If your skin has started to show wear and tear from sun, aging, acne or other skin conditions there is much that can be done.</p>
-                </div>
-                <div className='flex flex-col flex-wrap  items-center'>
-                    <div className='flex flex-col md:flex-row flex-wrap  items-center w-full justify-center md:justify-start md:pl-[30px] mt-10 ft:mt-[80px]'>
-                        <button className='bg-white text-[#796AB8] rounded-lg py-3 px-7 w-[249.61px] border-[1px] md:border-[0] font-bold hover:bg-[#ddd] md:mr-[30px] mb-[30px]'>VIEW TREATMENTS</button>
-                        <button className='bg-gradient-to-r from-[#E1C75C] to-[#C2A013] text-[#fff] rounded-lg py-3 px-7 w-[249.61px] font-bold hover:bg-gradient-to-l md:mr-[30px] mb-[30px]'>VIEW PRODUCTS</button>
-                    </div>
-                </div>
-                <div className='w-full flex flex-col items-center justify-center pb-3  px-3 absolute bottom-0 left-0 tb:bottom-20 tb:w-[60%] '
-                    style={{
-                        marginLeft: "auto",
-                        marginRight: "auto",
-                        left: "0",
-                        right: "0",
-                        textAlign: "center",
-                    }}>
-                    <div className='flex  flex-row items-center justify-between space-x-2 mt-[10px] md:mt-[100px] tb:mt-[200px] w-full'>
-                        {<a onClick={() => swiper.slidePrev()} href="#carouselExampleControls" data-slide="prev" className='carousel-control-prev'><BsChevronLeft size={25} className=' border-[1px] rounded-[50%] w-[40px] h-[40px] p-2 cursor-pointer hover:border-[#E8C229] hover:text-[#E8C229] navigationIcons mobile' /></a>}
-                        <div className='flex flex-row items-center space-x-3'>
-                            <img src='/assets/selectedEllipse.svg' />
-                            <img src='/assets/ellipse.svg' />
-                            <img src='/assets/ellipse.svg' />
-                        </div>
-                        {<a onClick={() => swiper.slideNext()} href="#carouselExampleControls" data-slide="next" className='carousel-control-next'> <BsChevronRight size={25} className='border-[1px] rounded-[50%] w-[40px] h-[40px] p-2 cursor-pointer hover:border-[#E8C229] navigationIcons mobile' /></a>}
-
-                    </div>
+                    <p className='md:max-w-[70%] lg:max-w-[550px] text-[17px] lg:text-[20px] text-white mt-10 px-4 lg:px-[0px] lg:mt-5'>If your skin has started to show wear and tear from sun, aging, acne or other skin conditions there is much that can be done.</p>
                 </div>
             </div>
         )
     }
 
     const Slide2 = () => {
-        const swiper = useSwiper();
         return (
-            <div className='self-center md:self-start md:ml-[70px]' >
-                <div className='self-center text-center md:text-start md:self-start  md:pl-[20px] mt-3 md:mt-4 w-full'>
+            <div className='self-center lg:self-start' >
+                <div className='flex flex-col md:items-center lg:items-start self-center text-center lg:text-start lg:self-start mt-3 lg:mt-4 w-full'>
                     <span className='text-white text-[60px] tb:text-[75px] font-bold leading-none'>PERFECTION <br />TO BE EPIC</span>
-                    <p className='md:max-w-[550px] text-[17px] md:text-[20px] text-white mt-10 px-3 md:px-[0px] md:mt-5'>If your skin has started to show wear and tear from sun, aging, acne or other skin conditions there is much that can be done.</p>
-                </div>
-                <div className='flex flex-col flex-wrap  items-center'>
-                    <div className='flex flex-col md:flex-row flex-wrap  items-center w-full justify-center md:justify-start md:pl-[13px] mt-10 ft:mt-[80px]'>
-                        <button className='bg-white text-[#796AB8] rounded-lg py-3 px-7 w-[249.61px] border-[1px] md:border-[0] font-bold hover:bg-[#ddd] md:mr-[30px] mb-[30px]'>VIEW TREATMENTS</button>
-                        <button className='bg-gradient-to-r from-[#E1C75C] to-[#C2A013] text-[#fff] rounded-lg py-3 px-7 w-[249.61px] font-bold hover:bg-gradient-to-l md:mr-[30px] mb-[30px]'>VIEW PRODUCTS</button>
-                    </div>
-                </div>
-                <div className='w-full flex flex-col items-center justify-center pb-3  px-3 absolute bottom-0 left-0 tb:bottom-20 tb:w-[60%] '
-                    style={{
-                        marginLeft: "auto",
-                        marginRight: "auto",
-                        left: "0",
-                        right: "0",
-                        textAlign: "center",
-                    }}>
-                    <div className='flex  flex-row items-center justify-between space-x-2 mt-[10px] md:mt-[100px] tb:mt-[200px] w-full'>
-                        {<a onClick={() => swiper.slidePrev()} href="#carouselExampleControls" data-slide="prev" className='carousel-control-prev'><BsChevronLeft size={25} className=' border-[1px] rounded-[50%] w-[40px] h-[40px] p-2 cursor-pointer hover:border-[#E8C229] hover:text-[#E8C229] navigationIcons mobile' /></a>}
-                        <div className='flex flex-row items-center space-x-3'>
-                            <img src='/assets/ellipse.svg' />
-                            <img src='/assets/selectedEllipse.svg' />
-                            <img src='/assets/ellipse.svg' />
-                        </div>
-                        {<a onClick={() => swiper.slideNext()} href="#carouselExampleControls" data-slide="next" className='carousel-control-next'> <BsChevronRight size={25} className='border-[1px] rounded-[50%] w-[40px] h-[40px] p-2 cursor-pointer hover:border-[#E8C229] navigationIcons mobile' /></a>}
-
-                    </div>
+                    <p className='md:max-w-[70%] lg:max-w-[550px] text-[17px] lg:text-[20px] text-white mt-10 px-4 lg:px-[0px] lg:mt-5'>If your skin has started to show wear and tear from sun, aging, acne or other skin conditions there is much that can be done.</p>
                 </div>
             </div>
         )
     }
     const Slide3 = () => {
+        return (
+            <div className='self-center md:items-center lg:items-start' >
+                <div className='flex flex-col items-center self-center text-center lg:text-start lg:self-start mt-3 lg:mt-4 w-full '>
+                    <span className='text-white text-[60px] tb:text-[75px] font-bold leading-none'>AMAZEMENT <br />TO BE EPIC</span>
+                    <p className='md:max-w-[70%] lg:max-w-[550px] text-[17px] lg:text-[20px] text-white mt-10 px-4 lg:px-[0px] lg:mt-5'>If your skin has started to show wear and tear from sun, aging, acne or other skin conditions there is much that can be done.</p>
+                </div>
+            </div>
+        )
+    }
+
+    const NavigationControls = () => {
         const swiper = useSwiper();
         return (
-            <div className='self-center md:self-start md:ml-[70px]' >
-                <div className='self-center text-center md:text-start md:self-start  md:pl-[20px] mt-3 md:mt-4 w-full'>
-                    <span className='text-white text-[60px] tb:text-[75px] font-bold leading-none'>AMAZEMENT <br />TO BE EPIC</span>
-                    <p className='md:max-w-[550px] text-[17px] md:text-[20px] text-white mt-10 px-3 md:px-[0px] md:mt-5'>If your skin has started to show wear and tear from sun, aging, acne or other skin conditions there is much that can be done.</p>
-                </div>
-                <div className='flex flex-col flex-wrap  items-center'>
-                    <div className='flex flex-col md:flex-row flex-wrap  items-center w-full justify-center md:justify-start md:pl-[13px] mt-10 ft:mt-[80px]'>
+            <>
+                <div className='flex flex-col flex-wrap  items-center w-full'>
+                    <div className='flex flex-col lg:flex-row flex-wrap  items-center w-full justify-start mt-10 ft:mt-[80px]'>
                         <button className='bg-white text-[#796AB8] rounded-lg py-3 px-7 w-[249.61px] border-[1px] md:border-[0] font-bold hover:bg-[#ddd] md:mr-[30px] mb-[30px]'>VIEW TREATMENTS</button>
                         <button className='bg-gradient-to-r from-[#E1C75C] to-[#C2A013] text-[#fff] rounded-lg py-3 px-7 w-[249.61px] font-bold hover:bg-gradient-to-l md:mr-[30px] mb-[30px]'>VIEW PRODUCTS</button>
                     </div>
@@ -119,7 +74,7 @@ const Homepage = () => {
 
                     </div>
                 </div>
-            </div>
+            </>
         )
     }
 
@@ -139,16 +94,19 @@ const Homepage = () => {
                 <SwiperSlide>
                     <PageHero background='/assets/homepageHeroSvg.svg' mobileBackground='./assets/mobileBackgroundSvg.svg'>
                         <Slide1 />
+                        <NavigationControls />
                     </PageHero>
                 </SwiperSlide>
                 <SwiperSlide>
                     <PageHero background='/assets/treatementsHeroSvg.svg' mobileBackground='/assets/mobileBackgroundSvg.svg'>
                         <Slide2 />
+                        <NavigationControls />
                     </PageHero>
                 </SwiperSlide>
                 <SwiperSlide>
                     <PageHero background='/assets/homepageHeroSvg.svg' mobileBackground='/assets/mobileBackgroundSvg.svg'>
                         <Slide3 />
+                        <NavigationControls />
                     </PageHero>
                 </SwiperSlide>
             </Swiper>
--- src/components/pageHero.jsx
+++ src/components/pageHero.jsx
@@ -8,10 +8,10 @@ function PageHero({ children, background, mobileBackground }) {
         <div className='flex flex-col w-full h-full'>
             <div>
                 <div className='relative  mb-7 w-full'>
-                    <img src={background} layout="fill" className="hidden md:flex w-full" />
+                    <img src={background} layout="fill" className="hidden lg:flex w-full" />
 
-                    <img src={mobileBackground} className='md:hidden w-full' />
-                    <div className='flex flex-col items-center  pt-20 top-0 absolute tb:top-[50px] w-full h-full'>
+                    <img src={mobileBackground} className='lg:hidden w-full' />
+                    <div className='flex flex-col items-center  pt-20 top-0 absolute tb:top-[50px] w-full h-full lg:ml-[70px]'>
                         {children}
                     </div>
                 </div>
